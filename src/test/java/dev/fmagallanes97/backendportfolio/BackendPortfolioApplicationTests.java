f5d1f6bd25fe1b67a40140c6f23c698c0ddedd3f
Facundo Magallanes <facundomagallanes97@gmail.com>
2022-11-02 08:55:28 -0300

    Add mysql instance in context test

--- src/test/java/dev/fmagallanes97/backendportfolio/BackendPortfolioApplicationTests.java
+++ src/test/java/dev/fmagallanes97/backendportfolio/BackendPortfolioApplicationTests.java
@@ -2,9 +2,25 @@ package dev.fmagallanes97.backendportfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class BackendPortfolioApplicationTests {
+
+	@Container
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.31");
+
+	@DynamicPropertySource
+	static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mysql::getJdbcUrl);
+		registry.add("spring.datasource.username", mysql::getUsername);
+		registry.add("spring.datasource.password", mysql::getPassword);
+	}
 
 	@Test
 	void contextLoads() {

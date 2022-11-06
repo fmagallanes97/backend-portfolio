0463b068be5ec004b7d97ebd2e890a0940e1a027
Facundo Magallanes <facundomagallanes97@gmail.com>
2022-11-06 11:30:27 -0300

    Add new find CRUD operation

--- src/main/java/dev/fmagallanes97/backendportfolio/positionresponsibility/ResponsibilityRepository.java
+++ src/main/java/dev/fmagallanes97/backendportfolio/positionresponsibility/ResponsibilityRepository.java
@@ -2,5 +2,8 @@ package dev.fmagallanes97.backendportfolio.positionresponsibility;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResponsibilityRepository extends JpaRepository<Responsibility, Long> {
+    public List<Responsibility> findAllByPositionId(Long id);
 }
--- src/main/java/dev/fmagallanes97/backendportfolio/skill/SkillRepository.java
+++ src/main/java/dev/fmagallanes97/backendportfolio/skill/SkillRepository.java
@@ -2,5 +2,8 @@ package dev.fmagallanes97.backendportfolio.skill;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+    public List<Skill> findAllByResumeId(Long id);
 }

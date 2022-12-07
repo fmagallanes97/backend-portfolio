c99544a27b0d8ac3f6f2f0bb27f1b776be5beecd
Facundo Magallanes <facundomagallanes97@gmail.com>
2022-12-07 09:00:40 -0300

    Add bidirectional relationship

--- src/main/java/dev/fmagallanes97/backendportfolio/position/Position.java
+++ src/main/java/dev/fmagallanes97/backendportfolio/position/Position.java
@@ -1,11 +1,14 @@
 package dev.fmagallanes97.backendportfolio.position;
 
+import dev.fmagallanes97.backendportfolio.positionresponsibility.PosResponsibility;
 import dev.fmagallanes97.backendportfolio.resume.Resume;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -25,4 +28,16 @@ public class Position {
     @ManyToOne
     @JoinColumn(name = "resume_id")
     private Resume resume;
+    @OneToMany(mappedBy = "position", orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<PosResponsibility> responsibilities = new HashSet<>();
+
+    public void addResponsibility(PosResponsibility responsibility) {
+        responsibilities.add(responsibility);
+        responsibility.setPosition(this);
+    }
+
+    public void removeResponsibility(PosResponsibility responsibility) {
+        responsibilities.remove(responsibility);
+        responsibility.setPosition(null);
+    }
 }

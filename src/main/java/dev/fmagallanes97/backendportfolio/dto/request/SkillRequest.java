ef01a864963b3c6bf2fc84dd815f1b29bff50892
Facundo Magallanes <facundomagallanes97@gmail.com>
2022-12-18 11:26:25 -0300

    Modify size constraint in name attr of skill request

--- src/main/java/dev/fmagallanes97/backendportfolio/dto/request/SkillRequest.java
+++ src/main/java/dev/fmagallanes97/backendportfolio/dto/request/SkillRequest.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public record SkillRequest(
-        @Size(min = 2, max = 45, message = "this value must be between 2 and 45 characters")
+        @Size(min = 4, max = 255, message = "this value must be between 4 and 255 characters")
         @NotEmpty(message = "this attribute is mandatory")
         String name
 ) {

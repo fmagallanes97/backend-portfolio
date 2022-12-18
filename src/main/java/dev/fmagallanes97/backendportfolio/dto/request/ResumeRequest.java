c0cff37891afba9dfb871559e40a5d7b95a791c4
Facundo Magallanes <facundomagallanes97@gmail.com>
2022-12-18 10:37:29 -0300

    Modify size constraint in headline attr of resume request

--- src/main/java/dev/fmagallanes97/backendportfolio/dto/request/ResumeRequest.java
+++ src/main/java/dev/fmagallanes97/backendportfolio/dto/request/ResumeRequest.java
@@ -10,7 +10,7 @@ public record ResumeRequest(
         @Size(min = 2, max = 45, message = "this value must be between 2 and 45 characters")
         @NotEmpty(message = "this attribute is mandatory")
         String lastName,
-        @Size(min = 2, max = 45, message = "this value must be between 2 and 45 characters")
+        @Size(min = 4, max = 45, message = "this value must be between 4 and 45 characters")
         @NotEmpty(message = "this attribute is mandatory")
         String headline,
         @Size(min = 20, max = 255, message = "this value must be between 20 and 255 characters")

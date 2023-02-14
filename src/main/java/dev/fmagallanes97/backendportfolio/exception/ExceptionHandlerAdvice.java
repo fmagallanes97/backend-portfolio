1f863abee6437e2d4c437e85a1a60b3c95fc31ce
fmagallanes97 <facundomagallanes97@gmail.com>
2023-02-14 18:14:05 -0300

    Add exceptions to the global exception handler

--- src/main/java/dev/fmagallanes97/backendportfolio/exception/ExceptionHandlerAdvice.java
+++ src/main/java/dev/fmagallanes97/backendportfolio/exception/ExceptionHandlerAdvice.java
@@ -4,9 +4,11 @@ import dev.fmagallanes97.backendportfolio.dto.response.InvalidArgumentResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.net.URI;
@@ -16,20 +18,21 @@ import java.util.List;
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ProblemDetail handle(NoHandlerFoundException ex) {
+    public ProblemDetail handleNoHandlerFoundException(NoHandlerFoundException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.NOT_FOUND,
-                "Testing..."
+                HttpStatus.BAD_REQUEST,
+                "The resource you try to access does not exist. Please check the documentation for the correct URI"
         );
 
-        problemDetail.setTitle("Not found");
-        problemDetail.setType(URI.create("http://localhost:8080/error/not-found"));
+        problemDetail.setTitle("Resource Not Exist");
+        problemDetail.setInstance(URI.create(ex.getRequestURL()));
+        problemDetail.setType(URI.create("https://example.com/error/resource-not-exist"));
 
         return problemDetail;
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ProblemDetail handleUnknownResource(ResourceNotFoundException ex) {
+    public ProblemDetail handleResourceNotFoundException(ResourceNotFoundException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
                 HttpStatus.NOT_FOUND,
                 ex.getMessage()
@@ -38,8 +41,34 @@ public class ExceptionHandlerAdvice {
         String resourceName = StringUtils.capitalize(ex.getResourceName());
 
         problemDetail.setTitle(resourceName + " Not found");
-        problemDetail.setType(URI.create("http://localhost:8080/error/resource-not-found"));
+        problemDetail.setType(URI.create("https://example.com/error/resource-not-found"));
         problemDetail.setProperty("id", ex.getResourceId());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                "The request parameter is invalid. Please check the documentation for the correct parameter value"
+        );
+
+        problemDetail.setTitle("Invalid Request Parameter");
+        problemDetail.setType(URI.create("https://example.com/error/invalid-parameter"));
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ProblemDetail handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.METHOD_NOT_ALLOWED,
+                "The HTTP method used to access the resource is not allowed. Please check the documentation for the correct HTTP method to use"
+        );
+
+        problemDetail.setTitle("Method Not Allowed");
+        problemDetail.setType(URI.create("https://example.com/error/method-not-allowed"));
 
         return problemDetail;
     }
@@ -54,7 +83,7 @@ public class ExceptionHandlerAdvice {
         );
 
         problemDetail.setTitle("Invalid Request Body");
-        problemDetail.setType(URI.create("http://localhost:8080/error/bad-request"));
+        problemDetail.setType(URI.create("https://example.com/error/validation-error"));
         problemDetail.setProperty("errors", arguments);
 
         return problemDetail;

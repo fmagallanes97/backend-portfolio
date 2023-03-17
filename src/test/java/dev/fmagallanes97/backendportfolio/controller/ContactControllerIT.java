98995b5ac10291e494db403e3e4c3a9542b1d7fe
fmagallanes97 <facundomagallanes97@gmail.com>
2023-03-17 09:46:35 -0300

    Modify contact controller integration test

--- src/test/java/dev/fmagallanes97/backendportfolio/controller/ContactControllerIT.java
+++ src/test/java/dev/fmagallanes97/backendportfolio/controller/ContactControllerIT.java
@@ -1,12 +1,8 @@
 package dev.fmagallanes97.backendportfolio.controller;
 
 import dev.fmagallanes97.backendportfolio.AbstractIntegrationTest;
-import dev.fmagallanes97.backendportfolio.dto.request.ResumeRequest;
-import dev.fmagallanes97.backendportfolio.service.ResumeService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -21,18 +17,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.profiles.active=test")
 class ContactControllerIT extends AbstractIntegrationTest {
 
     MockMvc mockMvc;
 
-    @BeforeAll
-    void setUpResume(@Autowired ResumeService service) {
-        ResumeRequest request = new ResumeRequest("John", "Doe", "Experienced Software Developer", "I am a highly skilled software developer with 5 years of experience in Java development.");
-        service.save(request);
-    }
+    final Long RESUME_ID = 1L;
 
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -42,20 +32,18 @@ class ContactControllerIT extends AbstractIntegrationTest {
     }
 
     @Test()
-    @Order(1)
     @DisplayName("Should verify that at least one resume can be retrieved")
     void exists_at_least_one_resume() throws Exception {
-        mockMvc.perform(get("/resume/1"))
+        mockMvc.perform(get("/resume/" + RESUME_ID))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
     }
 
     @Test
-    @Order(2)
     @DisplayName("Should verify that POST method works correctly")
     void contact_creation_works() throws Exception {
-        mockMvc.perform(post("/resume/1/contact")
+        mockMvc.perform(post("/resume/" + RESUME_ID + "/contact")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content("""
                                 {
@@ -67,13 +55,19 @@ class ContactControllerIT extends AbstractIntegrationTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
+
+        mockMvc.perform(get("/resume/" + RESUME_ID))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.contact[?(@.email == 'john.doe@example.com')]").exists())
+                .andExpect(jsonPath("$.contact[?(@.githubProfileURL == 'https://github.com/johndoe')]").exists())
+                .andExpect(jsonPath("$.contact[?(@.linkedinProfileURL == 'https://www.linkedin.com/in/johndoe/')]").exists());
     }
 
     @Test
-    @Order(3)
     @DisplayName("Should verify that request body infringe the constraints for POST method")
     void contact_creation_does_not_work() throws Exception {
-        mockMvc.perform(post("/resume/1/contact")
+        mockMvc.perform(post("/resume/" + RESUME_ID + "/contact")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content("""
                                 {
@@ -94,35 +88,32 @@ class ContactControllerIT extends AbstractIntegrationTest {
     }
 
     @Test
-    @Order(4)
     @DisplayName("Should verify that GET method works correctly")
     void contact_retrieving_works() throws Exception {
-        mockMvc.perform(get("/contact/1"))
+        mockMvc.perform(get("/contact/" + RESUME_ID))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
     }
 
     @Test
-    @Order(5)
     @DisplayName("Should verify that the path variable is not found for GET method")
     void contact_retrieving_does_not_work() throws Exception {
-        mockMvc.perform(get("/contact/9999"))
+        mockMvc.perform(get("/contact/" + -1))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
                 .andExpect(jsonPath("$.type").value("https://example.com/error/resource-not-found"))
                 .andExpect(jsonPath("$.title").value("Contact Not found"))
                 .andExpect(jsonPath("$.status").value(404))
-                .andExpect(jsonPath("$.detail").value("The contact with ID 9999 cannot be found in the database"))
-                .andExpect(jsonPath("$.id").value(9999));
+                .andExpect(jsonPath("$.detail").value("The contact with ID -1 cannot be found in the database"))
+                .andExpect(jsonPath("$.id").value(-1));
     }
 
     @Test
-    @Order(6)
     @DisplayName("Should verify that PUT method works correctly")
     void contact_updating_works() throws Exception {
-        mockMvc.perform(put("/contact/1")
+        mockMvc.perform(put("/contact/" + RESUME_ID)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content("""
                                 {
@@ -132,14 +123,14 @@ class ContactControllerIT extends AbstractIntegrationTest {
                                 }
                                 """))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
     }
 
     @Test
-    @Order(7)
     @DisplayName("Should verify that body request infringe the constrains for PUT method")
     void contact_updating_does_not_work() throws Exception {
-        mockMvc.perform(put("/contact/1")
+        mockMvc.perform(put("/contact/" + RESUME_ID)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content("""
                                 {
@@ -160,26 +151,24 @@ class ContactControllerIT extends AbstractIntegrationTest {
     }
 
     @Test
-    @Order(8)
     @DisplayName("Should verify that DELETE method works correctly")
     void contact_deleting_works() throws Exception {
-        mockMvc.perform(delete("/contact/1"))
+        mockMvc.perform(delete("/contact/" + RESUME_ID))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @Order(9)
     @DisplayName("Should verify that path variable is not found for DELETE method")
     void contact_deleting_does_not_work() throws Exception {
-        mockMvc.perform(delete("/contact/9999"))
+        mockMvc.perform(delete("/contact/" + -1))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
                 .andExpect(jsonPath("$.type").value("https://example.com/error/resource-not-found"))
                 .andExpect(jsonPath("$.title").value("Contact Not found"))
                 .andExpect(jsonPath("$.status").value(404))
-                .andExpect(jsonPath("$.detail").value("The contact with ID 9999 cannot be found in the database"))
-                .andExpect(jsonPath("$.id").value(9999));
+                .andExpect(jsonPath("$.detail").value("The contact with ID -1 cannot be found in the database"))
+                .andExpect(jsonPath("$.id").value(-1));
     }
 }
